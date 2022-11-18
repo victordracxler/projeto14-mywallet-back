d09905ccbd5fda2c4771af25a9a585703f5927aa
Victor Dracxler <victordracxler@poli.ufrj.br>
2022-11-17 23:54:33 -0300

    feat: insert user in db

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -4,6 +4,20 @@ import { signInSchema, signUpSchema } from '../index.js';
 export async function signUp(req, res) {
 	const user = req.body;
 
-	console.log(user);
-	res.sendStatus(200);
+	try {
+		const userExists = await usersCollection.findOne({ email: user.email });
+
+		if (userExists) {
+			res.status(401).send({ message: 'Email já cadastrado' });
+			return;
+		}
+
+		await usersCollection.insertOne(user);
+
+		console.log(user);
+		res.sendStatus(200);
+	} catch (err) {
+		console.log(err);
+		res.sendStatus(500);
+	}
 }
